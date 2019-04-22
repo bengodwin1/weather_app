974da58e1472b217dd8153628238fe58cbd79cb8
Ben Godwin <ben.godwin1@gmail.com>
2019-04-22 01:17:28 -0400

    Added form with user interactivity. Added current weather conditions and forecast. Added error handling for when Google API cannot recognize input

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,66 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Ben's Weather App" });
 });
 
-router.get('/current-temp', function(req, res, next) {
-  request("https://api.darksky.net/forecast/03cd6865c06f71f9b7f362c1d7b93660/39.9168,-75.3877/?exclude=[minutely,hourly,daily]", function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var parsedData = JSON.parse(body);
-        var temperature = parsedData["currently"]["temperature"];
-        console.log(temperature);
-        res.render('current-temp', { title: "Ben's Weather App", temp: temperature });
+router.post("/current-temp", function(req, res) {
+  address = req.body.address;
+  var lat = "";
+  var lng = "";
+  var fullAddress = "";
+  var reqStr =
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    address +
+    "&key=AIzaSyCuuDBglzNoBjwWhYtjgH6ZSXmASmkAvww";
+  request(reqStr, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var parsedData = JSON.parse(body);
+      console.log(parsedData);
+      console.log(parsedData["status"])
+      if (parsedData["status"] == "ZERO_RESULTS") {
+        res.render("error", {message: "ERROR: Location not found. Please try again"});
+        console.log("no address");
+        return;
       } else {
-        console.log("error");
-        res.render('error');
+        fullAddress = parsedData.results[0].formatted_address;
+        lat = parsedData.results[0].geometry.location.lat;
+        lng = parsedData.results[0].geometry.location.lng;
+        var forecastReqStr =
+          "https://api.darksky.net/forecast/03cd6865c06f71f9b7f362c1d7b93660/" +
+          lat +
+          "," +
+          lng;
+        request(forecastReqStr, function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var parsedData = JSON.parse(body);
+            var temperature = parsedData["currently"]["temperature"];
+            var current = parsedData["currently"]["summary"];
+            var forecast = parsedData["hourly"]["summary"];
+            var wind = parsedData["currently"]["windSpeed"];
+            var gusts = parsedData["currently"]["windGust"];
+            var highTemp = parsedData["daily"]["data"][0]["temperatureHigh"];
+            var lowTemp = parsedData["daily"]["data"][0]["temperatureLow"];
+            res.render("current-temp", {
+              title: "Ben's Weather App",
+              location: fullAddress,
+              temp: temperature,
+              location: fullAddress,
+              current: current,
+              forecast: forecast,
+              wind: wind,
+              gusts: gusts,
+              high: highTemp,
+              low: lowTemp
+            });
+          } else {
+            console.log("error");
+            res.render("error");
+          }
+        });
       }
-    });
+    } else {
+      console.log("error");
+    }
+  });
 });
-
 
 
 module.exports = router;
