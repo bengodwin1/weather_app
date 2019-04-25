83e4da4b48992801f69c166c92eecc0bac06e97b
Ben Godwin <ben.godwin1@gmail.com>
2019-04-25 16:15:47 -0400

    Centered weather div on the screen, added classes for background image based on weather

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,80 @@ var request = require('request');
 
 var url = 'mongodb://localhost:27017/test'
 
+function setWeatherIcon(str) {
+  if (str === clear-day) {
+    
+  }
+
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Ben's Weather App" });
+  var address = "Media, PA" ;
+  var lat = "";
+  var lng = "";
+  var fullAddress = "";
+  var reqStr =
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    address +
+    "&key=AIzaSyCuuDBglzNoBjwWhYtjgH6ZSXmASmkAvww";
+  request(reqStr, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var parsedData = JSON.parse(body);
+      if (parsedData["status"] == "ZERO_RESULTS") {
+        res.render("error", {message: "ERROR: Location not found. Please try again"});
+        return;
+      } else {
+        fullAddress = parsedData.results[0].formatted_address;
+        var s = fullAddress;
+        var k = s.indexOf(" ", s.indexOf(" ") + 1);
+        if (k !== -1) {
+          var result = s.slice(0, k);
+          if (result.charAt(result.length - 1) === ",") {
+            result = result.slice(0, result.length - 1);
+          }
+          fullAddress = result;
+        }
+        lat = parsedData.results[0].geometry.location.lat;
+        lng = parsedData.results[0].geometry.location.lng;
+        var forecastReqStr =
+          "https://api.darksky.net/forecast/03cd6865c06f71f9b7f362c1d7b93660/" +
+          lat +
+          "," +
+          lng;
+        request(forecastReqStr, function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var parsedData = JSON.parse(body);
+            var temperature = parsedData["currently"]["temperature"];
+            var current = parsedData["currently"]["summary"];
+            var forecast = parsedData["hourly"]["summary"];
+            var wind = parsedData["currently"]["windSpeed"];
+            var gusts = parsedData["currently"]["windGust"];
+            var highTemp = parsedData["daily"]["data"][0]["temperatureHigh"];
+            var lowTemp = parsedData["daily"]["data"][0]["temperatureLow"];
+            res.render("index", {
+              title: "Ben's Weather App",
+              location: fullAddress,
+              temp: temperature,
+              location: fullAddress,
+              current: current,
+              forecast: forecast,
+              wind: wind,
+              gusts: gusts,
+              high: highTemp,
+              low: lowTemp
+            });
+          } else {
+            res.render("error");
+          }
+        });
+      }
+    } else {
+    }
+  });
 });
 
-router.post("/current-temp", function(req, res) {
+router.post("/weather", function(req, res) {
   address = req.body.address;
   var lat = "";
   var lng = "";
@@ -24,16 +92,13 @@ router.post("/current-temp", function(req, res) {
   request(reqStr, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       var parsedData = JSON.parse(body);
-      console.log(parsedData);
-      console.log(parsedData["status"])
       if (parsedData["status"] == "ZERO_RESULTS") {
         res.render("error", {message: "ERROR: Location not found. Please try again"});
-        console.log("no address");
         return;
       } else {
         fullAddress = parsedData.results[0].formatted_address;
         var s = fullAddress;
-        var k = s.indexOf(" ", s.indexOf(" ") + 1);
+        var k = s.indexOf(" ", s.indexOf(",") + 2);
         console.log(k);
         if (k !== -1) {
           var result = s.slice(0, k);
@@ -59,7 +124,7 @@ router.post("/current-temp", function(req, res) {
             var gusts = parsedData["currently"]["windGust"];
             var highTemp = parsedData["daily"]["data"][0]["temperatureHigh"];
             var lowTemp = parsedData["daily"]["data"][0]["temperatureLow"];
-            res.render("current-temp", {
+            res.render("index", {
               title: "Ben's Weather App",
               location: fullAddress,
               temp: temperature,
@@ -72,13 +137,11 @@ router.post("/current-temp", function(req, res) {
               low: lowTemp
             });
           } else {
-            console.log("error");
             res.render("error");
           }
         });
       }
     } else {
-      console.log("error");
     }
   });
 });
